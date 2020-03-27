7561b8658abe966457e8ec8ca599256813cec815
Liam <liamreadsspam@gmail.com>
2020-03-27 00:01:13 -0300

    Update battlegrounds messaging facade

--- src/main/java/com/atherys/battlegrounds/facade/BattlegroundMessagingFacade.java
+++ src/main/java/com/atherys/battlegrounds/facade/BattlegroundMessagingFacade.java
@@ -1,31 +1,14 @@
 package com.atherys.battlegrounds.facade;
 
+import com.atherys.core.utils.AbstractMessagingFacade;
 import com.google.inject.Singleton;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.channel.MessageReceiver;
-import org.spongepowered.api.text.format.TextColors;
 
 @Singleton
-public class BattlegroundMessagingFacade {
-
-    public static final Text PREFIX = Text.of(TextColors.DARK_AQUA, "[", TextColors.DARK_PURPLE, "Battle", TextColors.DARK_AQUA, "] ", TextColors.RESET);
-
-    public Text formatInfo(Object... message) {
-        return Text.of(PREFIX, Text.of(message));
-    }
-
-    public Text formatError(Object... message) {
-        return Text.of(PREFIX, TextColors.RED, Text.of(message));
-    }
-
-    public void info(MessageReceiver messageReceiver, Object... message) {
-        messageReceiver.sendMessage(formatInfo(message));
-    }
-
-    public void error(MessageReceiver messageReceiver, Object... message) {
-        messageReceiver.sendMessage(formatError(message));
+public class BattlegroundMessagingFacade extends AbstractMessagingFacade {
+    public BattlegroundMessagingFacade() {
+        super("Battle");
     }
 
     public void broadcast(Object... message) {
